6672665cb025dab4bfc16c2241584619ff12424e
Sathish9686 <princesathish9686@gmail.com>
2023-07-20 13:00:38 +0530

    final-commit

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 
 app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "Sathishmysql",
-  password: "Sathish@mysql321",
-  database: "sampledatabase"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 const initializeDbAndServer = () => {
